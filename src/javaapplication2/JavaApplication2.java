eec8f52de9cab590ddc076eeac0f9290e23e06b6
Estudiante <Estudiante@10.203.1.66>
2018-02-06 09:28:45 -0500

    version 1

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class JavaApplication2 {
+
+    public static void main(String[]arg){
+        System.out.println("hola mundo");
+    }
+    
+}
